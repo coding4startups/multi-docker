866f34e6e7bf4d22fd7e43b514acf7689617bc1e
coding4startups <coding4startups@gmail.com>
2020-01-02 17:07:30 +0100

    map function

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -34,9 +34,9 @@ class Fib extends Component {
     this.setState({ index: '' });
   };
 
-  renderSeenIndexes() {
-    return this.state.seenIndexes.map(({ number }) => number).join(', ');
-  }
+  // renderSeenIndexes() {
+    // return this.state.seenIndexes.map(({ number }) => number).join(', ');
+  // }
 
   renderValues() {
     const entries = [];
